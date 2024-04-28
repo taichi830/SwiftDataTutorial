422c16f6cb48598afc47bb39bc785a67b1d97474
taichiro <taicloud0803@icloud.com>
2024-04-29 00:30:24 +0900

    stateで保存できる所まで

--- SwiftDataTutorial/ContentView.swift
+++ SwiftDataTutorial/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends: [Friend] = [
+        Friend(name: "Elton Lin", birthday: .now),
+        Friend(name: "Jenny Court", birthday: Date(timeIntervalSince1970: 0))
+    ]
+    
+    @State private var newName: String = ""
+    @State private var newDate: Date = Date.now
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(friends, id: \.name) { friend in
+                HStack {
+                    Text(friend.name)
+                    Spacer()
+                    Text(friend.birthday, format: .dateTime.month(.wide).day().year())
+                }
+            }
         }
-        .padding()
+        .navigationTitle("Birthdays")
+        .safeAreaInset(edge: .bottom) {
+            VStack(alignment: .center, spacing: 20) {
+                Text("New Birthday")
+                    .font(.headline)
+                DatePicker(selection: $newDate, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                    TextField("Name", text: $newName)
+                        .textFieldStyle(.roundedBorder)
+                }
+                Button("Save") {
+                    let newFriend = Friend(name: newName, birthday: newDate)
+                    friends.append(newFriend)
+                    
+                    newName = ""
+                    newDate = .now
+                }
+                .bold()
+            }
+            .padding()
+            .background(.bar)
+        }
     }
 }
 
---
+++ SwiftDataTutorial/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  SwiftDataTutorial
+//
+//  Created by fukuda taichiro on 2024/04/28.
+//
+
+import Foundation
+
+struct Friend {
+    let name: String
+    let birthday: Date
+}
