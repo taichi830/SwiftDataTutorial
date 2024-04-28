2d766017e24b95130f2243bd7b45ea6992ce0ca6
taichiro <taicloud0803@icloud.com>
2024-04-28 19:24:22 +0900

    first commit

---
+++ SwiftDataTutorial/SwiftDataTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataTutorialApp.swift
+//  SwiftDataTutorial
+//
+//  Created by fukuda taichiro on 2024/04/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
